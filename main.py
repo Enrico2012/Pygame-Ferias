277e0f01124f7ad9e86dda1acf9f65d763a37be8
João <joaoeduardosouza1@gmail.com>
2023-08-01 09:05:25 -0300

    marcadores_tela

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import random
 
 pygame.init()
 
-width = 600
-height = 750
+width = 900
+height = 600
 tam_tela = (width, height)
 tela = pygame.display.set_mode(tam_tela)
 pygame.display.set_caption('Highway Rush!')
@@ -15,3 +15,11 @@ verde = (76, 208, 56)
 verm = (200, 0, 0)
 branco = (255, 255, 255)
 amar = (255, 232, 0)
+
+marcador_largura = 10
+marcador_altura = 50
+
+rua = (100, 0, 300, height)
+borda_esquerda = (95, 0, marcador_largura, height)
+borda_direita = (395, 0, marcador_largura, height)
+
